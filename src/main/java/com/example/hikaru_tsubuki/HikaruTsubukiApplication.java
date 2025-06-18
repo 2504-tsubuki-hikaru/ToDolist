22ea14de91189c2f577fde0e5c58b924307763d6
tsubuki_hikaru <tsubuki.hikaru@alhinc.jp>
2025-06-18 14:08:55 +0900

    コミット

---
+++ src/main/java/com/example/hikaru_tsubuki/HikaruTsubukiApplication.java
@@ -0,0 +1,13 @@
+package com.example.hikaru_tsubuki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikaruTsubukiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikaruTsubukiApplication.class, args);
+	}
+
+}
